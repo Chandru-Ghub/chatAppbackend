948bccf7f3a9309c4fd64ba10b6889ac5af4bacf
Chandru-Ghub <chandruinfo455@gmail.com>
2023-09-14 13:13:46 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const app = express()
+require('dotenv').config()
+const http = require('http')
+const PORT = process.env.port;
+const cors = require('cors');
+const {Server, Socket} = require('socket.io')
+
+app.use(cors())
+
+const server = http.createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin:'https://harmonious-gaufre-34e64e.netlify.app/',
+        methods:['GET','POST']
+    }
+})
+
+io.on('connection',(socket)=>{
+    // console.log(socket.id);
+
+    socket.on('join_room',(data)=>{
+        socket.join(data);
+        console.log('user_id: '+ socket.id);
+        console.log('room: '+data);
+    })
+
+
+    socket.on('send_message',(data)=>{
+        socket.to(data.room).emit('received_msg',data)
+        console.log(data);
+    })
+
+    socket.on('disconnect',()=>{
+        console.log('user Disconnected',socket.id);
+    })
+})
+
+server.listen(PORT,()=>{
+    console.log('server running in port ',PORT);
+})
